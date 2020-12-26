62b206b80d4ceb745fae41cf75f843bc5a31b876
aris-creator <56575042+aris-creator@users.noreply.github.com>
2020-12-26 13:01:24 +0000

    Update checkout.js

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-
 import { SiteContext, ContextProviderComponent } from "../context/mainContext"
 import { DENOMINATION } from "../../providers/inventoryProvider"
 import { FaLongArrowAltLeft } from "react-icons/fa"
