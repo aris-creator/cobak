8b34f3a1a49185fb4770216e72f9676bb634ec28
aris-creator <56575042+aris-creator@users.noreply.github.com>
2021-01-12 06:06:12 +0000

    Update lambda.js

--- snippets/lambda.js
+++ snippets/lambda.js
@@ -1,51 +1,64 @@
-'use strict'
+// https://stripe.com/docs/payments/without-card-authentication
+const stripe = require("stripe")("API_KEY")
 
-const ApiConfig = require('./apiConfig');
-const HttpClient = require('./httpClient');
-const Transaction = require('./transaction');
-/**
- * Snap object used to do request to Midtrans Snap API
- */
-class Snap{
-  constructor(options={isProduction:false,serverKey:'',clientKey:''}){
-    this.apiConfig = new ApiConfig(options);
-    this.httpClient = new HttpClient(this);
-    this.transaction = new Transaction(this);
+exports.handler = async event => {
+  if (!event.body || event.httpMethod !== "POST") {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        status: "invalid http method",
+      }),
+    }
   }
 
-  /**
-   * Do `/transactions` API request to Snap API
-   * @param  {Object} parameter - object of Core API JSON body as parameter, will be converted to JSON (more params detail refer to: https://snap-docs.midtrans.com)
-   * @return {Promise} - Promise contains Object from JSON decoded response
-   */
-  createTransaction(parameter={}){
-    let apiUrl = this.apiConfig.getSnapApiBaseUrl()+'/transactions';
-    let responsePromise = this.httpClient.request(
-      'post',
-      this.apiConfig.get().serverKey,
-      apiUrl,
-      parameter);
-    return responsePromise;
+  const order = JSON.parse(event.body)
+
+  const calculateOrderAmount = items => {
+    // Replace this constant with a calculation of the order's amount
+    // You should always calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400
   }
-  /**
-   * Wrapper function that call `createTransaction` then:
-   * @return {Promise} - Promise of String token
-   */
-  createTransactionToken(parameter={}){
-    return this.createTransaction(parameter)
-      .then(function(res){
-        return res.token;
+
+  try {
+    const intent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(order.items),
+      currency: "idr",
+      payment_method: order.payment_method_id,
+
+      // A PaymentIntent can be confirmed some time after creation,
+      // but here we want to confirm (collect payment) immediately.
+      confirm: true,
+
+      // If the payment requires any follow-up actions from the
+      // customer, like two-factor authentication, Stripe will error
+      // and you will need to prompt them for a new payment method.
+      error_on_requires_action: true,
+    })
+
+    if (intent.status === "succeeded") {
+      // This creates a new Customer and attaches the PaymentMethod in one API call.
+      const customer = await stripe.customers.create({
+        payment_method: intent.payment_method,
+        email: order.email,
+        address: order.address,
       })
-  }
-  /**
-   * Wrapper function that call `createTransaction` then:
-   * @return {Promise} - Promise of String redirect_url
-   */
-  createTransactionRedirectUrl(parameter={}){
-    return this.createTransaction(parameter)
-      .then(function(res){
-        return res.redirect_url;
-      })
+      // Handle post-payment fulfillment
+      console.log(`Created Payment: ${intent.id} for Customer: ${customer.id}`)
+      // Now ship those goodies
+      await inventoryAPI.ship(order)
+    } else {
+      // Any other status would be unexpected, so error
+      console.log({ error: "Unexpected status " + intent.status })
+    }
+  } catch (e) {
+    if (e.type === "StripeCardError") {
+      // Display error to customer
+      console.log({ error: e.message })
+    } else {
+      // Something else happened
+      console.log({ error: e.type })
+    }
   }
 }
-module.exports = Snap;
