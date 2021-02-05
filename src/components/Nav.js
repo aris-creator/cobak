430d6b9983d6e6525641fe4042d3ff783bc595cd
aris-creator <56575042+aris-creator@users.noreply.github.com>
2021-02-05 00:07:31 +0000

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import styled from "styled-components"
 import { titleIfy, slugify } from '../../utils/helpers'
 import { FaShoppingCart, FaCircle } from 'react-icons/fa';
 import { Link } from "gatsby"
