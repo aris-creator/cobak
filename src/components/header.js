e27e5fa2d46262af9d8be4acde8d02b3d8e1c7e6
aris-creator <56575042+aris-creator@users.noreply.github.com>
2021-02-10 21:06:11 +0000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import React from "react"
 import { GiHamburgerMenu } from "react-icons/gi"
 import { IoCloseSharp } from "react-icons/io5"
+import navBar from './src/components/Navbarlink'
+import './src/css/site.css
 
 const Header = () => {
   const [menuState, setMenuState] = useState(false);
