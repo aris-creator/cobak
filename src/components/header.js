0e6fce066e5027d2b01e324507e834d8e85d2ad9
aris-creator <56575042+aris-creator@users.noreply.github.com>
2021-02-10 20:29:26 +0000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ const Header = () => {
           <div className="text-4xl cursor-pointer">P.</div>
         </$Link>
       </div>
-      <Categoryview menuState={menuState} setMenuState={setMenuState} />
+      <Navbarlink menuState={menuState} setMenuState={setMenuState} />
       <ul className="text-lg items-center hidden md:flex">
         <li>
           <Link activeClassName="a-header" href="/">
