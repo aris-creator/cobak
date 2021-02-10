8f717d4ce0e9753a235eea680811321a376c0d9d
aris-creator <56575042+aris-creator@users.noreply.github.com>
2021-02-10 20:25:55 +0000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { GiHamburgerMenu } from "react-icons/gi";
+import { GiHamburgerMenu } from "react-icons/gi"
+import { IoCloseSharp } from "react-icons/io5"
 
 const Header = () => {
   const [menuState, setMenuState] = useState(false);
