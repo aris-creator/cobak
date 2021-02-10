c793e0cc27214a7f56b3a46ae35b6c9ab3a68e07
aris-creator <56575042+aris-creator@users.noreply.github.com>
2021-02-10 20:13:51 +0000

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,73 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => {
+  const [menuState, setMenuState] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuState(!menuState);
+  };
+
+  return (
+    <div style={{ height: "10vh" }} className="px-10 flex justify-between">
+      <div className="flex items-center">
+        <Link activeClassName="text-third" href="/">
+          <div className="text-4xl cursor-pointer">P.</div>
+        </$Link>
+      </div>
+      <MenuOverlay menuState={menuState} setMenuState={setMenuState} />
+      <ul className="text-lg items-center hidden md:flex">
+        <li>
+          <Link activeClassName="a-header" href="/">
+            <a className="header mr-5">About</a>
+          </$Link>
+        </li>
+        <li>
+          <Link activeClassName="a-header" href="/inventory">
+            <a className="header">Menu</a>
+          </$Link>
+        </li>
+        <li>
+          <Link activeClassName="a-header" href="/overview">
+            <a className="header">Burger Station</a>
+          </$Link>
+        </li>
+        <li>
+          <Link activeClassName="a-header" href="/overview">
+            <a className="header">Donuts Station</a>
+          </$Link>
+        </li>
+        <li>
+          <Link activeClassName="a-header" href="/overview">
+            <a className="header">Jajan Pasar Station</a>
+          </$Link>
+        </li>
+        <li>
+          <Link activeClassName="a-header" href="/overview">
+            <a className="header">Our Store</a>
+          </$Link>
+        </li>
+      </ul>
+      <div className="flex items-center md:hidden z-10">
+        {menuState ? (
+          <IoCloseSharp
+            onClick={handleToggleMenu}
+            fill="#fff"
+            size={26}
+            className="menu-close cursor-pointer transition-all"
+          />
+        ) : (
+          <GiHamburgerMenu
+            onClick={handleToggleMenu}
+            size={23}
+            className="cursor-pointer transition-all"
+          />
+        )}
+      </div>
     </div>
-  </header>
-)
+  );
+};
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
